3e14d309b4a112bf8471986f2b1dff33022923ee
artem20002909 <99039253+artem20002909@users.noreply.github.com>
2025-01-13 09:53:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-class K17_08:
-  def __init__(self, a1, a2=11):
-      self.p1 = a1
-      self.p2 = a2
-ob1 = K17_08("принтер", "P1006")
-ob2 = K17_08("windows ;(")
-print(ob1.p1, ob1.p2)
-print(ob2.p1, ob2.p2)
+class K17_10:
+    def __init__(self , a1, a2, a3):
+        self.p1 = a1
+        self.p2 = a2
+        self.p3 = a3
+        self.funcl ()
+    def funcl(self):
+        s = (self.p2*self.p3)/2
+        print(self.p1, s)
+
+ob1 = K17_10("площя 1 = ", 3, 4)
+ob2 = K17_10("площя 2=", 5, 6)
+ob1.form = "перший зафарбований"
+print(ob1.form)
