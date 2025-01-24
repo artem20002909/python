d2c377e6fc8d724be0104b9b3e87e8ad80129b9f
artem20002909 <99039253+artem20002909@users.noreply.github.com>
2025-01-24 10:03:37 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,17 +1,22 @@
-class K18_04c:
-    def __init__(self, a1):
+class K18_06:
+    def __init__(self, a1, a2):
         self.p1 = a1
-    def __add__(self, a2):
-        self.p1 = self.p1 + a2
-    def __str__ (self):
-        return '%s' % self.p1
+        self.p2 = a2
+    def func1(self):
+        self.p1 =self.p1 / self.p2
+        print(self.p1)
 
-ob1 = K18_04c(21)
-ob1 + 15
-print(ob1)
-ob2 = K18_04c("...")
-ob2 + "download"
-print(ob2)
-ob3 = K18_04c([19, 7, 12])
-ob3 + [5, 9]
-print(ob3)
+class K18_06a(K18_06):
+    def func1(self):
+        print("перехід 1")
+        K18_06.func1(self)
+        print("перехід 2")
+
+i = 1
+while i < 7:
+    if 2 < i and i < 5:
+        ob1 = K18_06a(7, i)
+    else:
+        ob1 = K18_06(10, i)
+    i += 1
+    ob1.func1()
