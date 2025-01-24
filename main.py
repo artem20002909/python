bb48a4ccad24f455175d87e07a5d38d824a29e89
artem20002909 <99039253+artem20002909@users.noreply.github.com>
2025-01-24 10:52:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import math
+class K18_16:
+    def __init__(self, a1, a2):
+        try:
+            self.z = a1 / a2
+        except ZeroDivisionError:
+            print("0")
+        else:
+            print("result=", self.z)
+        finally:
+            print("program stop")
 
-class K18_07a:
-    def __init__(self, x):
-        self.plcolo = 3.14 * x * x
-class K18_07b:
-    def __init__(self, x):
-        self.pltrik = math.sqrt(3)
-class K18_07:
-    def __init__(self, x, y):
-        self.plzag = x * y
-    def func1(self, r1, r2, a, m=2, n=2):
-        self.p1 = K18_07a(r1)
-        self.p2 = K18_07a(r2)
-        self.p3 = K18_07b(a)
-        self.k1 = m
-        self.k2 = n
-    def func2(self):
-        self.func2 = self.plzag - self.p1.plcolo * self.k1\
-            -self.p2.plcolo * self.k2 - self.p3.pltrik
-    def func3(self):
-        print("загальна площя = ", str(self.plzag), "см.квадратних")
-        print("залишок площі", str(self.func2), "см.квадратних")
-
-ob = K18_07(30, 20)
-ob.func1(4, 6, 5)
-ob.func2()
-ob.func3()
+x = int(input("x"))
+v = int(input("v"))
