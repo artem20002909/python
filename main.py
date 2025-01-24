2b418e19c8fe325ab5af82e847c30449a6afca2f
artem20002909 <99039253+artem20002909@users.noreply.github.com>
2025-01-24 09:55:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-class K17_12:
-  contr = int(input("mass :"))
-  def __init__(self, a1, a2, a3):
-    self.p1 = a1
-    self.p2 = a2
-    self.p3 = a3
-  def func1(self):
-    v = self.p1 * self.p2 * self.p3
-    print("ob`em", m, " =", v)
+class K18_04:
+    def __init__(self, a1):
+        self.p = a1
+    def func1(self):
+        print("K18_04 =", self.p - 13)
 
-class K17_12a(K17_12):
-  def func2(self, vaga):
-    if vaga < K17_12.contr:
-      self.p4 = "mojno "
-    else:
-      self.p4 = " ne mojno:("
-  def func3(self):
-    print(self.p4)
+class K18_04a(K18_04):
+    def func2(self, a2):
+        self.p *= a2
+        print("K18_04a =", self.p)
 
-ob1 = K17_12a(2, 3, 4)
-m = "1"
+class K18_04b(K18_04):
+    def func3(self):
+        self.func3 = list(str(self.p))
+        print("list=", self.func3)
+    def func1(self):
+        i = 0
+        while i < len(self.func3):
+            print(i, "element:", self.func3[i])
+            i += 1
+
+ob1 = K18_04a(37)
+ob2 = K18_04b('byte')
+ob1.func2(3)
 ob1.func1()
-ob2 = K17_12(3, 4, 5)
-m = "2"
+ob2.func3()
 ob2.func1()
-ob1.func2(5)
-ob1.func3()
