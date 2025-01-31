9bf39183a72e919e42f6f8b5d0fafabc15358282
artem20002909 <99039253+artem20002909@users.noreply.github.com>
2025-01-31 10:44:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-class K18_16:
-    def __init__(self, a1, a2):
-        try:
-            self.z = a1 / a2
-        except ZeroDivisionError:
-            print("0")
-        else:
-            print("result=", self.z)
-        finally:
-            print("program stop")
+from tkinter import *
 
-x = int(input("x"))
-v = int(input("v"))
+root = Tk()
+
+def func1(event):
+    s = ent.get()
+    if s == "1234":
+        lb1.configure(text="Access granted")
+        lb2.destroy()
+        ent.destroy()
+        but = Button(root, bg="red", text="balance")
+        but.bind("<Button-1>", func2)
+        but.pack()
+    else:
+        lb1.configure(text="Wrong password. Stopped")
+
+def func2(event):
+    lb1.configure(text="Balance is 1000")
+
+lb1 = Label(root, bg='red', text="Enter password", font="Arial 10")
+lb1.grid(padx=30, pady=1)
+
+lb2 = Label(root, text="")
+lb2.grid()
+
+ent = Entry(root, width=40, bg='yellow')
+ent.grid()
+ent.bind("<Return>", func1)
+
+root.mainloop()
